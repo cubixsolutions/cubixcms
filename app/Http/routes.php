5330bfb68520fff50ee611a9d0d634dcb683ac69
Sean Pollock <sean@cubix-solutions.com>
2015-03-27 17:25:41 -0600

    removed new route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,17 +10,6 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-Route::get('/checkstatus', function() {
-
-   $env = app->detectEnvironment(function() {
-
-      return getenv('APP_ENV') ?: 'LOCAL';
-
-   });
-
-    echo $env;
-
-});
 
 Route::get('/', 'WelcomeController@index');
 
