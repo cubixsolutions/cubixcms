d93357f99adedcc2dfd11e2895919fdaa3e2676f
Sean Pollock <sean@cubix-solutions.com>
2015-03-27 21:35:54 -0600

    no comment

---
+++ resources/views/our-pledge.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.default')
+
+@section('head')
+
+
+@stop
+
+@section('content')
+
+<div class="jumbotron">
+    
+    	<div class="container">
+        
+        	<h1>Our Pledge</h1>
+            <h3>Our commitment to you</h3>
+            
+        </div>
+    
+    </div>
+        
+    <div class="container">
+    
+    	<div class="row">
+        
+        	<div class="col-md-12">
+            
+            <div class="panel panel-info">
+            
+            	<div class="panel-body">
+                                	
+                    <p>Our customers are very important to us and we aim to provide high quality services, exceed customer expectations and delight our customers. We will treat you as individuals, listen carefully to you, be sensitive to your needs, and take ownership of problems so that you can be confident that someone is dealing with your issue.</p>
+                	<strong>What you can expect from us...</strong><br /><br />
+                    <strong>We will:</strong><br /><br />
+                    <ul>
+                    
+                    	<li>be friendly, polite, helpful, attentive and considerate at all times;</li>
+                        <li>provide accurate and clear information;</li>
+                        <li>use plain English and avoid jargon or technical terms;</li>
+                        <li>be open and honest about what we can and cannot deliver;</li>
+                        <li>get back to you when we say we will, and advise you of our progress when we are taking longer than anticipated to address an issue.</li>
+                    
+                    </ul>
+                    <br />
+                    <p>We always aim to get it right first time, every time, so that you don’t have to keep contacting us about the same issue.</p> 
+
+<p>We also aim to ensure we understand the real problem and not only help to fix it but also to ensure that we get to the root cause of the issue to avoid it keep happening again, either to you or to someone else.</p>
+
+<p>We always promise to make it easy for you to contact us and to respond to ‘phone calls, emails, letters and other requests for service as quickly as possible. Our priority is always to deliver a quality service and this can take time. We will do everything we can to keep in contact with you, if it is necessary to do so, and keep you informed of progress.</p>
+
+<p>If we have no choice but to pass your enquiry on to someone else we will tell you who will be dealing with it, and we will support you to get the help you need.</p>
+
+<p>If your enquiry cannot be dealt with at the time you contact us we will make sure you know what is happening and when you can expect to receive further contact from us.</p>
+
+
+                </div>
+            
+            </div>
+            
+            </div>
+        
+        </div>
+    
+    </div>
+
+    @stop
