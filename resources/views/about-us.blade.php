64f4cd0c658e0a9f5bb4c766670d1419b8b155c3
Sean Pollock <sean@cubix-solutions.com>
2015-03-27 21:33:57 -0600

    no comment

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,134 @@
+@extends('layouts.default')
+
+@section('content')
+
+<div class="jumbotron">
+    
+    	<div class="container">
+        
+        	<h1>About Us</h1>
+        	<h3>Everything to know about Cubix Solutions</h3>
+        </div>
+   
+    </div>
+    
+    <div class="container">
+    
+    	<div class="row">
+        
+        	<div class="col-md-8">
+     
+                 <div class="panel panel-default">
+                 
+                 	<div class="panel-body">
+                    	<p>Cubix Solutions is based in Bloomfield, New Mexico.  Cubix Solutions is a service company offering IT Services to the Four Corners area of New Mexico.  Cubix Solutions started operations in early 2014 and before that went under the name of WebBuzzDesign who offered web development and hosting services.  Since we changed names in early 2014 we have expanded our business to include PC Repair and Printing Services.</p>
+                        
+                        <p>Our senior Tech, Sean Pollock, who is also the owner of Cubix Solutions has had an extensive background in Computers dating back to the mid 80's when he got his first computer, a Tandy 1000 TX and spent many hours learning to program and repair computers;  In addition to being self-taught he has a degree in Information Technology and holds several industry certifications including A+ Certification.</p>
+                    	                        
+                        <p>Our current service offerings include PC Repair, Web Development, Web Hosting, Website Management, SEO Services, Printing Services and so much more. </p>
+                        
+                        <p>Since Cubix Solutions inception we have always had a strong workethic and have always strived for perfection in all our work.</p>
+                        
+                    </div>
+                 
+                 </div>
+                               
+            </div>
+            
+            <div class="col-md-4">
+            
+            	<div class="panel panel-info">
+                
+                	<div class="panel-heading">
+                    	<span class="fa fa-phone"></span> How to contact us
+                    </div>
+                    
+                    <div class="panel-body">
+                    
+                    	<div class="list-group">
+                        
+                        	<a href="#" class="list-group-item">
+                            
+                            	<h4 class="list-group-item-heading"><span class="fa fa-phone"></span> By Phone</h4>
+                                <p class="list-group-item-text">505.609.8142</p>
+                                
+                            </a>
+                            
+                            <a href="#" class="list-group-item">
+                            
+                            	<h4 class="list-group-item-heading"><span class="fa fa-fax"></span> By Fax</h4>
+                                <p class="list-group-item-text">877.365.7633</p>
+                                                    
+                            </a>
+                            
+                            <a href="#" class="list-group-item">
+                            	
+                            	<h4 class="list-group-item-heading"><span class="fa fa-at"></span> By Email</h4>
+                                	
+                                    <div class="list-group-item">
+                                    
+                                    	<h4 class="list-group-item-heading">Tech Support</h4>
+                                    	<p class="list-group-item-text">&#115;&#117;&#112;p&#111;&#114;&#116;&#64;&#99;u&#98;&#105;&#120;-so&#108;utio&#110;s.co&#109;
+</p>
+                                    	                                    
+                                    </div>
+                                    
+                                    <div class="list-group-item">
+                                    
+                                    	<h4 class="list-group-item-heading">Billing</h4>
+                                    	<p class="list-group-item-text">&#98;&#105;&#108;li&#110;&#103;&#64;&#99;&#117;bi&#120;-&#115;&#111;lu&#116;ions.co&#109;
+</p>
+                                    	                                    
+                                    </div>
+                                    
+                                    <div class="list-group-item">
+                                    
+                                    	<h4 class="list-group-item-heading">Sales</h4>
+                                		<p class="list-group-item-text">&#115;&#97;&#108;&#101;s&#64;&#99;&#117;&#98;&#105;&#120;-s&#111;lu&#116;io&#110;s.co&#109;
+</p>
+                                    
+                                    </div>
+                                    
+                                    <div class="list-group-item">
+                                    
+                                    	<h4 class="list-group-item-heading">TOS Abuse</h4>
+                                    	<p class="list-group-item-text">&#097;&#098;&#117;&#115;&#101;&#064;&#099;&#117;&#098;&#105;&#120;&#045;&#115;&#111;&#108;&#117;&#116;&#105;&#111;&#110;&#115;&#046;&#099;&#111;&#109;</p>
+                                    </div>
+                                	
+                                
+                                
+                                
+                            
+                            </a>
+                            
+                            <a href="#" class="list-group-item">
+                            
+                            	<h4 class="list-group-item-heading"><span class="fa fa-envelope"></span> By Postal Mail</h4>
+                                <p class="list-group-item-text">
+                                
+                                <address>
+                                	<strong>Cubix Solutions</strong><br />
+                                    105 W. Main St.<br />
+                                    Bloomfield, NM  87413</p>
+                                </address>
+                            
+                            
+                            </a>
+                        
+                        </div>
+                        
+                    </div>
+                
+                </div>
+            
+            </div>
+        
+        
+        </div>
+    
+    
+    
+    
+    </div>
+
+@stop
