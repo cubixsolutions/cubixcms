58dd1dbe19975dab571d84e79b986e70e4cf8ccd
Sean Pollock <sean@cubix-solutions.com>
2015-03-28 00:55:27 -0600

    no comment

--- app/Http/Controllers/pageController.php
+++ app/Http/Controllers/pageController.php
@@ -9,12 +9,13 @@ class pageController extends Controller {
 
     public function index($slug = 'home') {
 
-        die('slug: ' . $slug);
+        //die('slug: ' . $slug);
         $this->page($slug);
     }
 
     public function page($slug) {
 
+        die($slug);
        if(view()->exists($slug)) {
 
            return view($slug);
