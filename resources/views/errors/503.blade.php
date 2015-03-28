dfc467a0f6057d01ffc3599d90719dc4440286cc
Sean Pollock <sean@cubix-solutions.com>
2015-03-28 16:56:51 -0600

    no comment

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,7 +1,7 @@
 
 <html>
 	<head>
-        <title>{{$title - $slogan}}</title>
+        <title>{{$title}} - {{$slogan}}</title>
 		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
 		<style>
