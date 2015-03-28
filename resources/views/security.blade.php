643bed5ef029b3856cb58d42bd581dfb90e7ff1d
Sean Pollock <sean@cubix-solutions.com>
2015-03-27 23:15:59 -0600

    no comment

---
+++ resources/views/security.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.default')
+
+@section('content')
+
+<div class="jumbotron">
+    
+    	<div class="container">
+        
+        	<h1>Security</h1>
+        	<h3>Your information is very important to us</h3>
+        </div>
+   
+</div>
+    
+<div class="container">
+    
+   	<div class="row">
+        
+        	<div class="col-md-12">
+
+                <div class="panel panel-info">
+
+                    <div class="panel-body">
+
+                        <h1>Security of Your Personal Information</h1>
+
+                        <p>Cubix Solutions takes security very seriously.  Our platform uses the most powerful security tools that exist in the marketplace and all sensitive information, including bank account and credit card information, is stored encrypted.</p>
+                        <p>The Cubix Solutions website and all Cubix Solutions modules, including hosted webforms, use a 256-bit DigiCert certificate, but accommodate the majority of browsers at 128-bit encryption.</p>
+                        <p>Processing is performed by SSL (Secured Socket Layer) and compliant with PCI DSS - Payment Card Industry Data Security Standards.</p>
+                        <h1>PCI DSS Certified</h1>
+                        <img src="/_assets/pci-dss-compliant.gif" />
+                        <p>Payment Card Industry Data Security Standard (PCI DSS) has become one of the most important advances in the credit card industry and online security, and is now required by Visa/MasterCard for all merchants handling credit cards. Not following these rules can result in fines to the merchant and processing privileges being suspended.</p>
+                        <p>All credit card data is stored encrypted, and cannot be decrypted-except during the process of transmitting a transaction.</p>
+
+
+                    </div>
+
+                </div>
+        
+        
+            </div>
+       
+    </div>
+
+</div>
+
+@stop
