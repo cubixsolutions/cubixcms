c16831c6043305630af239c2a8f385b14c81920a
Sean Pollock <sean@cubix-solutions.com>
2015-04-01 09:30:07 -0600

    added Stripe webhook controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,7 +29,7 @@
     Route::post('store/add-cart/{id}','storeController@addCart');
     Route::post('store/update-cart','storeController@updateCart');
 
-    Route::post('webhooks','Webhooks.Stripe.stripeWebhookController@handleWebhook');
+    Route::post('webhooks','Laravel\Cashier\WebhookController@handleWebhook');
 
     Route::controllers([
         'auth' => 'Auth\AuthController',
