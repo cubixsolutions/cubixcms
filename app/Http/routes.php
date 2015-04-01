f966605db5eb221cf0b287d76d8998ada2147d1a
Sean Pollock <sean@cubix-solutions.com>
2015-04-01 10:44:04 -0600

    no comment

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,11 +29,12 @@
     Route::post('store/add-cart/{id}','storeController@addCart');
     Route::post('store/update-cart','storeController@updateCart');
 
+    Route::post('webhooks','Laravel\Cashier\WebhookController@handleWebhook');
+
     //Route::post('webhooks','Laravel\Cashier\WebhookController@handleWebhook');
 
     Route::controllers([
         'auth' => 'Auth\AuthController',
-        'password' => 'Auth\PasswordController',
-        'webhook'   => 'Webhooks\StripeWebhookController',
+        'password' => 'Auth\PasswordController'
 
     ]);
