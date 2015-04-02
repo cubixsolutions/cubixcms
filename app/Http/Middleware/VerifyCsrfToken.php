a7d25763fc99d78156ad945d53468a6ca2c0a4f1
Sean Pollock <sean@cubix-solutions.com>
2015-04-02 05:45:25 -0600

    no comment

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,7 +14,20 @@ class VerifyCsrfToken extends BaseVerifier {
 	 */
 	public function handle($request, Closure $next)
 	{
-		return parent::handle($request, $next);
+		//disable CSRF check on following routes
+        $skip = array(
+                'webhooks');
+
+        foreach ($skip as $key => $route) {
+
+            if($request->is($route)) {
+
+                return parent::addCookieToResponse($request, $next($request));
+
+            }
+        }
+
+        return parent::handle($request, $next);
 	}
 
 }
