f17b795d05f8c2ef0440e555970474f084ca4412
Sean Pollock <sean@cubix-solutions.com>
2015-04-02 05:16:25 -0600

    no comment

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,7 +29,7 @@
     Route::post('store/add-cart/{id}','storeController@addCart');
     Route::post('store/update-cart','storeController@updateCart');
 
-    Route::post('Webhooks', 'Webhooks/StripeWebhookController@handleWebhook');
+    Route::post('webhooks', 'Webhooks/StripeWebhookController@handleWebhook');
 
     //Route::post('webhooks','Laravel\Cashier\WebhookController@handleWebhook');
 
