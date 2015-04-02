328a7a48134b006ac52459f01634a4209029aa2a
Sean Pollock <sean@cubix-solutions.com>
2015-04-02 06:24:37 -0600

    no comment

--- app/Http/Controllers/Webhooks/StripeWebhookController.php
+++ app/Http/Controllers/Webhooks/StripeWebhookController.php
@@ -22,7 +22,7 @@ class StripeWebhookController extends WebhookController {
     public function handleWebhook()
     {
         $payload = $this->getJsonPayload();
-
+        dd($payload['id']);
         if (! $this->eventExistsOnStripe($payload['id'])) {
             return;
         }
