e278b1eb4373944181c0b2304a88d1313764f8b9
Sean Pollock <sean@cubix-solutions.com>
2015-04-02 06:06:15 -0600

    no comment

--- app/Http/Controllers/Webhooks/StripeWebhookController.php
+++ app/Http/Controllers/Webhooks/StripeWebhookController.php
@@ -1,7 +1,7 @@
 <?php namespace App\Http\Controllers\Webhooks;
 
 use Laravel\Cashier\WebhookController;
-
+use App\Http\Requests\Request;
 class StripeWebhookController extends WebhookController {
 
     /*
