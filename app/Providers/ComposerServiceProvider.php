9f39feef1afc41d1793804d731b94c95df7f3275
Sean Pollock <sean@cubix-solutions.com>
2015-05-13 05:30:50 -0600

    updated checkout page but is only 50% completed.

--- app/Providers/ComposerServiceProvider.php
+++ app/Providers/ComposerServiceProvider.php
@@ -13,7 +13,7 @@ class ComposerServiceProvider extends ServiceProvider {
     public function boot( Setting $setting)
     {
 
-
+        /*
         $setting = $setting->all();
 
         View::composer('*', function($view) use($setting) {
@@ -26,7 +26,7 @@ class ComposerServiceProvider extends ServiceProvider {
            }
 
         });
-
+        */
     }
 
     /**
